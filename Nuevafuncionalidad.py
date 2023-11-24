f02f08a4eb7079e8fafc071b34a974cfbb898980
Isabel Yebra <isabel.yebra@educa.madrid.org>
2023-11-24 09:32:46 +0100

    Commit 3

--- Nuevafuncionalidad.py
+++ Nuevafuncionalidad.py
@@ -1,2 +1,2 @@
-print ("Imagino que en este archivo está el código de una nueva funcionalidad");
+print("Imagino que en este archivo está el código de una nueva funcionalidad");
 print("Añado código ....")
